f54261df78cc8e424de8ef1d72494e579f966cce
MATHEWMESFIN <mathewmesfin@gmail.com>
2024-01-18 14:34:36 -0500

    updated skills section

--- src/app/components/Skills.jsx
+++ src/app/components/Skills.jsx
@@ -1,82 +1,31 @@
 'use client'
 import React, { useState, useTransition } from 'react';
 import skills from './skillsData';
-import TabButton from './TabButton';
 import styled from 'styled-components';
 
-const SkillsSection = styled.div`
- 
-`
-
-const SkillsWrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-`
-
-const SkillPanel = styled.div`
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    width: 25rem;
+const SkillsContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    width: 100%;
     height: 20rem;
-    align-items: center;
-
-    .active-tab {
-        position: absolute;
-        width: 90%;
-        height: var(--skill-height);
-        border-radius: 1rem;
-        background-color: rgba(139, 92, 246, 0.2);
-        transform: translateY(var(--distance));
-        transition: transform 0.3s ease-out;
-    }
-
-    @media (max-width: 1120px) {
-        width: 15rem;
-    }
-
-    @media (max-width: 880px) {
-        width: 10rem;
-        height: 30rem;
-
-        .active-tab {
-            height: var(--skill-height-small);
-            transform: translateY(var(--distance));
-        
-        }
-    }
-`
-
-const SkillTypeContainer = styled.div`
-    position: relative;
+    gap: 1rem;
+    padding: 1rem;
     background-color: rgba(139, 92, 246, 0.2);
     border-radius: 1rem;
-    width: 100%;
-    height: 20rem;
+    overflow: auto;
+
+    @media (max-width: 1120px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
 
     @media (max-width: 880px) {
         height: 30rem;
-    }
-`
-
-const Skill = styled.div`
-    position: absolute;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    overflow: auto;
-    gap: 1rem;
-    width: 100%;
-    height: 100%;
-    border: none;
-    border-top-right-radius: 1rem;
-    border-bottom-right-radius: 1rem;
-    padding: 1rem;
-    transform: translateY(var(--skill-offset));
-    transition: all 0.3s ease-in;
-
-    @media (max-width: 1120px) {
-        grid-template-columns: 1fr;
         gap: 0.5rem;
+    }
+
+    @media (max-width: 640px) {
+        grid-template-columns: repeat(1, 1fr);
+    
     }
 `
 
@@ -89,14 +38,26 @@ const SkillItem = styled.div`
     border-radius: 0.5rem;
     padding: 0.5rem 1rem;
     gap: 1rem;
-    background-color: rgba(139, 92, 246, 0.5);
+    background-color: rgba(139, 92, 246, 0.75);
     color: white;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+        transform: scale(1.1);
+        background-color: rgba(139, 92, 246);
+
+        .skill-item-img {
+            transform: rotate(360deg);
+        }
+    }
 
     .skill-item-img {
         font-size: 2rem;
+        transition: all 0.3s ease-in-out;
     }
 
-    @media (max-width: 1120px) {
+    @media (max-width: 880px) {
         font-size: 0.75rem;
         padding: 0.25rem 0.5rem;
         gap: 0.5rem;
@@ -106,74 +67,21 @@ const SkillItem = styled.div`
                 font-size: 1.5rem;
             }
     }
-
 `
 
-const Skills = () => {
-    const [tab, setTab] = useState(1);
-    const [isPending, startTransition] = useTransition();
-
-    const handleTabChange = (id) => {
-        startTransition(() => {
-            setTab(id)
-        })
-    }
-    return (
-        <SkillsSection>
-            <SkillsWrapper>
-                <SkillPanel >
-                    <div 
-                        className='active-tab'
-                        style={{
-                            "--distance": `${tab - 1}00%`,
-                            "--skill-height": `${20 / 3}rem`,
-                            "--skill-height-small": `${30 / 3}rem`,
-                        }}
-                        />
-                    <TabButton 
-                        active={tab === 1}
-                        selectTab={() => handleTabChange(1)}
-                        title='Frontend' />
-                    <TabButton 
-                        active={tab === 2}
-                        selectTab={() => handleTabChange(2)}
-                        title='Backend' />
-                    <TabButton 
-                        active={tab === 3}
-                        selectTab={() => handleTabChange(3)}
-                        title='Other' />
-                </SkillPanel>
-                <SkillTypeContainer>
-                
-                    {skills.map((skill) => (
-                        <SkillCard key={skill.id} skill={skill} tab={tab} />
-                    ))}
-
-                </SkillTypeContainer>
-
-            </SkillsWrapper>
-        </SkillsSection>
-    )
-}
-
-const SkillCard = ({ skill, tab }) => {
+export const Skills = () => {
 
     return (
-        <Skill
-            style={{
-                "--skill-offset": tab === skill.id ? '0' : tab > skill.id ? '30%' : '-30%',
-                zIndex: tab === skill.id ? 1 : 0,
-                opacity: tab === skill.id ? '1' : '0'
-            }}>
-                {skill.skills.map((item) => (
-                    <SkillItem key={item.id}>
-                        <div className='skill-item-img'>
-                            {item.image}
-                        </div>
-                        {item.name}
-                    </SkillItem>
-                ))}
-        </Skill>
+        <SkillsContainer>
+            {skills.map((skill) => (
+                <SkillItem className='skill-item' key={skill.id}>
+                    <div className='skill-item-img'>
+                        {skill.image}
+                    </div>
+                    {skill.name}
+                </SkillItem>
+            ))}
+        </SkillsContainer>
     )
 }
 
--- src/app/components/skillsData.jsx
+++ src/app/components/skillsData.jsx
@@ -1,155 +1,136 @@
-import { SiTailwindcss, SiNextdotjs, SiReact, SiBootstrap, SiHtml5, 
+import { SiTailwindcss, SiNextdotjs, SiReact, SiHtml5, 
     SiCss3, SiJavascript, SiNodedotjs, SiExpress, SiMongodb, SiMysql, 
-    SiPostgresql, SiPython, SiPhp, SiSass, SiStyledcomponents, SiGit, SiGithub,
-    SiFigma, SiVercel, SiStreamlit, SiPandas, SiFolium } from "react-icons/si";
+    SiPostgresql, SiPython, SiPhp, SiSass, SiStyledcomponents, SiGit, SiGithub, 
+    SiVercel, SiStreamlit, SiPandas, SiFolium, SiAxios, SiPrisma, SiJira } from "react-icons/si";
 import { TbBrandVscode } from "react-icons/tb";
-
-
 import { DiJava } from "react-icons/di";
 
 const skills = [
     {
         id: 1,
-        title: "Frontend",
-        skills: [
-            {
-                id: 1,
-                name: "React",
-                image: [<SiReact />]
-            },
-            {
-                id: 2,
-                name: "Next.js",
-                image: [<SiNextdotjs />]
-            },
-            {
-                id: 3,
-                name: "Tailwind",
-                image: [<SiTailwindcss />]
-            },
-            {
-                id: 4,
-                name: "Bootstrap",
-                image: [<SiBootstrap />]
-            },
-            {
-                id: 5,
-                name: "HTML",
-                image: [<SiHtml5 />]
-            },
-            {
-                id: 6,
-                name: "CSS",
-                image: [<SiCss3 />]
-            },
-            {
-                id: 7,
-                name: "JavaScript",
-                image: [<SiJavascript />]
-            },
-            {
-                id: 8,
-                name: "Sass",
-                image: [<SiSass />]
-            },
-            {
-                id: 10,
-                name: "styled-components",
-                image: [<SiStyledcomponents />]
-            }
-        ]
+        name: "React",
+        image: [<SiReact />]
     },
     {
         id: 2,
-        title: "Backend",
-        skills: [
-            {
-                id: 1,
-                name: "Node.js",
-                image: [<SiNodedotjs />]
-            },
-            {
-                id: 2,
-                name: "Express",
-                image: [<SiExpress />]
-            },
-            {
-                id: 3,
-                name: "MongoDB",
-                image: [<SiMongodb />]
-            },
-            {
-                id: 4,
-                name: "MySQL",
-                image: [<SiMysql />]
-            },
-            {
-                id: 5,
-                name: "PostgreSQL",
-                image: [<SiPostgresql />]
-            },
-            {
-                id: 6,
-                name:"Python",
-                image: [<SiPython />]
-            },
-            {
-                id: 7,
-                name: "Java",
-                image: [<DiJava />]
-            },
-            {
-                id: 8,
-                name: "PHP",
-                image: [<SiPhp />]
-            },
-            {
-                id: 9,
-                name: "Streamlit",
-                image: [<SiStreamlit />]
-            },
-            {
-                id: 10,
-                name: "Pandas",
-                image: [<SiPandas />]
-            },
-            {
-                id: 11,
-                name: "Folium",
-                image: [<SiFolium />]
-            }
-        ]
+        name: "Next.js",
+        image: [<SiNextdotjs />]
     },
     {
         id: 3,
-        title: "Other",
-        skills: [
-            {
-                id: 1,
-                name: "Git",
-                image: [<SiGit />]
-            },
-            {
-                id: 2,
-                name: "GitHub",
-                image: [<SiGithub />]
-            },
-            {
-                id: 3,
-                name: "VS Code",
-                image: [<TbBrandVscode />]
-            },
-            {
-                id: 4,
-                name: "Figma",
-                image: [<SiFigma />]
-            },
-            {
-                id: 5,
-                name: "Vercel",
-                image: [<SiVercel />]
-            }
-        ]
+        name: "Tailwind",
+        image: [<SiTailwindcss />]
+    },
+    {
+        id: 4,
+        name: "HTML",
+        image: [<SiHtml5 />]
+    },
+    {
+        id: 5,
+        name: "CSS",
+        image: [<SiCss3 />]
+    },
+    {
+        id: 6,
+        name: "JavaScript",
+        image: [<SiJavascript />]
+    },
+    {
+        id: 7,
+        name: "Sass",
+        image: [<SiSass />]
+    },
+    {
+        id: 8,
+        name: "styled-components",
+        image: [<SiStyledcomponents />]
+    },
+    {
+        id: 9,
+        name: "Node.js",
+        image: [<SiNodedotjs />]
+    },
+    {
+        id: 10,
+        name: "Express",
+        image: [<SiExpress />]
+    },
+    {
+        id: 11,
+        name: "MongoDB",
+        image: [<SiMongodb />]
+    },
+    {
+        id: 12,
+        name: "MySQL",
+        image: [<SiMysql />]
+    },
+    {
+        id: 13,
+        name: "PostgreSQL",
+        image: [<SiPostgresql />]
+    },
+    {
+        id: 14,
+        name:"Axios",
+        image: [<SiAxios />]
+
+    },
+    {
+        id: 15,
+        name:"Prisma",
+        image: [<SiPrisma />]
+    },
+    {
+        id: 16,
+        name:"Python",
+        image: [<SiPython />]
+    },
+    {
+        id: 17,
+        name: "Java",
+        image: [<DiJava />]
+    },
+    {
+        id: 18,
+        name: "PHP",
+        image: [<SiPhp />]
+    },
+    {
+        id: 19,
+        name: "Streamlit",
+        image: [<SiStreamlit />]
+    },
+    {
+        id: 20,
+        name: "Pandas",
+        image: [<SiPandas />]
+    },
+    {
+        id: 21,
+        name: "Folium",
+        image: [<SiFolium />]
+    },
+    {
+        id: 22,
+        name: "Git",
+        image: [<SiGit />]
+    },
+    {
+        id: 23,
+        name: "GitHub",
+        image: [<SiGithub />]
+    },
+    {
+        id: 24,
+        name: "Vercel",
+        image: [<SiVercel />]
+    },
+    {
+        id: 25,
+        name: "Jira",
+        image: [<SiJira />]
     }
 ]
 
